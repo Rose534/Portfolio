fbb249a902fea458abce895a0774caa22c150840
rose534 <wambuirose534@gmail.com>
2023-03-01 04:15:41 +0300

    navigation bar

--- chatterbox-client/src/components/Navigation.js
+++ chatterbox-client/src/components/Navigation.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-function NavigationBar() {
+function Navigation() {
   return (
-    <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light" style={{backgroundColor: '#f5f5f5'}}>
+    <nav id="navbar" className="navbar navbar-expand-lg navbar-light bg-light">
         <img id = "logo" src="https://53525363.000webhostapp.com/Images/PORTLO-removebg-preview.png" alt="PORTLO" />
       <div className="container">
         
-        <a className="navbar-brand" href="#">PORTLO</a>
+        <a className="navbar-brand" href="#"></a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
+            <li id ="navbar-pages" className="nav-item">
               <a className="nav-link" href="#home">Home</a>
             </li>
             <li className="nav-item">
@@ -28,4 +28,4 @@ function NavigationBar() {
   );
 }
 
-export default NavigationBar;
+export default Navigation;
