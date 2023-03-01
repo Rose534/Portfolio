f6b97cf480b2aa92e004d27dfb65066d7baff7d9
rose534 <wambuirose534@gmail.com>
2023-03-01 16:56:54 +0300

    migrate

--- chatterbox-server/db/migrate/20230301134021_create_projects.rb
+++ chatterbox-server/db/migrate/20230301134021_create_projects.rb
@@ -5,8 +5,6 @@ class CreateProjects < ActiveRecord::Migration[6.1]
       t.text :description
       t.string :image_url
       
-  
-
       t.timestamps
     end
   end
--- chatterbox-server/db/migrate/20230301134113_create_posts.rb
+++ chatterbox-server/db/migrate/20230301134113_create_posts.rb
@@ -1,6 +1,5 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
-    def change
       create_table :blog_posts do |t|
         t.string :title
   
