c4e329d48c3460602f473485f44febfeecb6eb43
rose534 <wambuirose534@gmail.com>
2023-03-01 17:32:12 +0300

    database population

--- chatterbox-server/db/seeds.rb
+++ chatterbox-server/db/seeds.rb
@@ -1,2 +1,5 @@
-Project.create(title: "Project 1", description: "This is project 1", image_url: "https://example.com/project1.png")
-Project.create(title: "Project 2", description: "This is project 2", image_url: "https://example.com/project2.png")
+Project.create(title: "Project 1", description: "This is project 1", image_url: "https://example.com/project1.png", created_at: Time.now, updated_at: Time.now)
+Project.create(title: "Project 2", description: "This is project 2", image_url: "https://example.com/project2.png", created_at: Time.now, updated_at: Time.now)
+
+Post.create(title: "My First Blog Post", created_at: Time.now, updated_at: Time.now)
+Post.create(title: "My Second Blog Post", created_at: Time.now, updated_at: Time.now)
