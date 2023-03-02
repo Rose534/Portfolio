292bdfa338b671a4af90244d9b364603be7ab7b1
rose534 <wambuirose534@gmail.com>
2023-03-02 18:14:37 +0300

    react home page design

--- chatterbox-client/src/components/Home.js
+++ chatterbox-client/src/components/Home.js
@@ -5,8 +5,13 @@ function Home() {
 
     return(
         <div>
-           
-            <img id= "girl" src="https://53525363.000webhostapp.com/Images/Pretty_puff_girl-removebg-preview.png"/>
+           <img id= "girl" src="https://53525363.000webhostapp.com/Images/Pretty_puff_girl-removebg-preview.png" alt=""/>
+           <div id="hello">
+            <h1>HELLO,</h1>
+            <h3>I am an ILLUSTRATOR, GRAPHIC DESIGNER </h3>
+            <h3> </h3>
+            <h3>and UI/UX designer</h3>
+           </div>
         </div>
     )
 
