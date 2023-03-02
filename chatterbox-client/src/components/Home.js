4bb692be2b019502fc4951d8f56fd1768f0ad613
rose534 <wambuirose534@gmail.com>
2023-03-02 19:35:39 +0300

    homepage css

--- chatterbox-client/src/components/Home.js
+++ chatterbox-client/src/components/Home.js
@@ -8,8 +8,9 @@ function Home() {
            <img id= "girl" src="https://53525363.000webhostapp.com/Images/Kezia_Gabriella_-_Anxiety-removebg-preview.png" alt=""/>
            <div id="hello">
             <h1 id="helo"> HELLO,</h1>
-            <h3 id= "illustrator">I am an ILLUSTRATOR, GRAPHIC DESIGNER </h3>
-            <h3 id= "ui">and UI/UX designer</h3>
+            <h3 id= "illustrator">I am an <mark>ILLUSTRATOR,</mark> <mark> GRAPHIC DESIGNER </mark></h3>
+            
+            <h3 id= "ui">and <mark>UI/UX </mark>designer</h3>
            </div>
            <div>
           {/*   <p id= "dreams"> Bringing your <br>
