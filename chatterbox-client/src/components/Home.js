91b0cca461dea05c90eda9f0f2024a74234c614c
rose534 <wambuirose534@gmail.com>
2023-03-03 03:09:13 +0300

    homepage css

--- chatterbox-client/src/components/Home.js
+++ chatterbox-client/src/components/Home.js
@@ -21,6 +21,20 @@ function Home() {
            <a href="https://www.instagram.com/illustrationsbywambui/" target="_blank"><img src="https://53525363.000webhostapp.com/Images/icons8-instagram-48-removebg-preview.png" alt=""/></a>
            <a href="https://mail.google.com/mail/u/0/#inbox?compose=DmwnWrRmTNxNvmpKRVHmQwkWPdlQFvwshQVPrrkDBVBbSwnhTngPHPjTWwrhtJLqFqQhhcPrgsDL" target="_blank"><img src="https://53525363.000webhostapp.com/Images/icons8-mail-48-removebg-preview.png" alt=""/></a>
            </div>
+           <div>
+            <h1 id="div2">About Me</h1>
+            <p id="rose">My name is Rose, I am a designer from Nairobi, Kenya with 3 years of experience. <br></br>
+                Below are some of the projects i've worked on, thanks for stopping by.</p>
+           </div>
+           <div id="div3">
+           <img src="https://53525363.000webhostapp.com/Images/Genie_Espinosa_on_Behance-removebg-preview.png" alt=""/>
+           </div>
+           <div id="div4">
+            <h1>Skills</h1>
+            <img id="img1" src="https://53525363.000webhostapp.com/Images/icons8-user-24.png" alt=""/>
+            <img id="img2" src="https://53525363.000webhostapp.com/Images/icons8-prototyping-64.png" alt=""/>
+            <img id="img3" src="https://53525363.000webhostapp.com/Images/icons8-design-50.png" alt=""/>
+           </div>
         </div>
     )
 
