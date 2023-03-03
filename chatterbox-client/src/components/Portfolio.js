3b828072f7f0e9a94332b8dfe9dd0933e205f5d5
rose534 <wambuirose534@gmail.com>
2023-03-03 19:52:06 +0300

    portfolio fetch and design

--- chatterbox-client/src/components/Portfolio.js
+++ chatterbox-client/src/components/Portfolio.js
@@ -1,14 +1,40 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Portfolio() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rose-portfolio-sinatra.onrender.com/projects')
+      .then(response => response.json())
+      .then(data => setProjects(data));
+  }, []);
+
+  const handleDelete = (id) => {
+    fetch(`https://rose-portfolio-sinatra.onrender.com/projects/${id}`, { method: 'DELETE' })
+      .then(response => response.json())
+      .then(data => setProjects(prevProjects => prevProjects.filter(project => project.id !== data.id)))
+  };
+
   return (
-    <div>
-        <h1>"portfolio"</h1>
+    <div className="container">
+      <h1 className="text-center mb-5">Illustrations By Rose</h1>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {projects.map(project => (
+          <div className="col-md-4 project" key={project.id}>
+            <div className="project-container">
+              <img className="project-image img-fluid" src={project.image_url} alt={project.title} />
+              <div className="project-overlay">
+                <button className="delete-btn" onClick={() => handleDelete(project.id)}>X</button>
+              </div>
+              <div className="project-details">
+                <h5>{project.title}</h5>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
-  )
-    
-  
+  );
 }
 
 export default Portfolio;
