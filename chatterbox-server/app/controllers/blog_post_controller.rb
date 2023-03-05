59a03999835e21900213318e4552b34eb5098c81
rose534 <wambuirose534@gmail.com>
2023-03-05 17:48:55 +0300

    app

--- chatterbox-server/app/controllers/blog_post_controller.rb
+++ chatterbox-server/app/controllers/blog_post_controller.rb
@@ -1,41 +1,41 @@
-class BlogPostsController < ApplicationController
+# class BlogPostsController < ApplicationController
     
-  #   post '/blog_posts' do
-  #     blog_post = BlogPost.create(params)
-  #     if blog_post.valid?
-  #       redirect "/blog_posts/#{blog_post.id}"
-  #     else
+#     post '/blog_posts' do
+#       blog_post = BlogPost.create(params)
+#       if blog_post.valid?
+#         redirect "/blog_posts/#{blog_post.id}"
+#       else
        
-  #     end
-  #   end
+#       end
+#     end
   
     
-  #   get '/blog_posts' do
-  #     @blog_posts = BlogPost.all
-  #     erb :'blog_posts/index'
-  #   end
+#     get '/blog_posts' do
+#       @blog_posts = BlogPost.all
+#       erb :'blog_posts/index'
+#     end
   
     
-  #   get '/blog_posts/:id' do
-  #     @blog_post = BlogPost.find(params[:id])
-  #     erb :'blog_posts/show'
-  #   end
+#     get '/blog_posts/:id' do
+#       @blog_post = BlogPost.find(params[:id])
+#       erb :'blog_posts/show'
+#     end
   
   
-  #   patch '/blog_posts/:id' do
-  #     blog_post = BlogPost.find(params[:id])
-  #     blog_post.update(params)
-  #     if blog_post.valid?
-  #       redirect "/blog_posts/#{blog_post.id}"
-  #     else
+#     patch '/blog_posts/:id' do
+#       blog_post = BlogPost.find(params[:id])
+#       blog_post.update(params)
+#       if blog_post.valid?
+#         redirect "/blog_posts/#{blog_post.id}"
+#       else
         
-  #     end
-  #   end
+#       end
+#     end
   
     
-  #   delete '/blog_posts/:id' do
-  #     BlogPost.destroy(params[:id])
-  #     redirect '/blog_posts'
-  #   end
-  # end
+#     delete '/blog_posts/:id' do
+#       BlogPost.destroy(params[:id])
+#       redirect '/blog_posts'
+#     end
+#   end
   
