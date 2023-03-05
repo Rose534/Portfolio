362ce7155fd6aac74a98d656b4704480a82d8280
rose534 <wambuirose534@gmail.com>
2023-03-05 17:04:02 +0300

    blog post

--- chatterbox-server/app/controllers/blog_post_controller.rb
+++ chatterbox-server/app/controllers/blog_post_controller.rb
@@ -1,41 +1,41 @@
-# class BlogPostsController < ApplicationController
-#     # Create
-#     post '/blog_posts' do
-#       blog_post = BlogPost.create(params)
-#       if blog_post.valid?
-#         redirect "/blog_posts/#{blog_post.id}"
-#       else
-#         # Handle validation errors
-#       end
-#     end
+class BlogPostsController < ApplicationController
+    # Create
+    post '/blog_posts' do
+      blog_post = BlogPost.create(params)
+      if blog_post.valid?
+        redirect "/blog_posts/#{blog_post.id}"
+      else
+        # Handle validation errors
+      end
+    end
   
-#     # Read all
-#     get '/blog_posts' do
-#       @blog_posts = BlogPost.all
-#       erb :'blog_posts/index'
-#     end
+    # Read all
+    get '/blog_posts' do
+      @blog_posts = BlogPost.all
+      erb :'blog_posts/index'
+    end
   
-#     # Read one
-#     get '/blog_posts/:id' do
-#       @blog_post = BlogPost.find(params[:id])
-#       erb :'blog_posts/show'
-#     end
+    # Read one
+    get '/blog_posts/:id' do
+      @blog_post = BlogPost.find(params[:id])
+      erb :'blog_posts/show'
+    end
   
-#     # Update
-#     patch '/blog_posts/:id' do
-#       blog_post = BlogPost.find(params[:id])
-#       blog_post.update(params)
-#       if blog_post.valid?
-#         redirect "/blog_posts/#{blog_post.id}"
-#       else
-#         # Handle validation errors
-#       end
-#     end
+    # Update
+    patch '/blog_posts/:id' do
+      blog_post = BlogPost.find(params[:id])
+      blog_post.update(params)
+      if blog_post.valid?
+        redirect "/blog_posts/#{blog_post.id}"
+      else
+        # Handle validation errors
+      end
+    end
   
-#     # Delete
-#     delete '/blog_posts/:id' do
-#       BlogPost.destroy(params[:id])
-#       redirect '/blog_posts'
-#     end
-#   end
+    # Delete
+    delete '/blog_posts/:id' do
+      BlogPost.destroy(params[:id])
+      redirect '/blog_posts'
+    end
+  end
   
