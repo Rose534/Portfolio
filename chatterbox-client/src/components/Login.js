338caf359e147deebfe9e07623c54f2c430864b9
rose534 <wambuirose534@gmail.com>
2023-03-05 19:54:02 +0300

    login

--- chatterbox-client/src/components/Login.js
+++ chatterbox-client/src/components/Login.js
@@ -1,26 +1,5 @@
 import React, { useState } from 'react';
 
-function App() {
-  const [loggedIn, setLoggedIn] = useState(false);
-  const handleLogin = (username) => {
-    setLoggedIn(true);
-    console.log(`${username} logged in`);
-  };
-
-  return (
-    <div className="App">
-      {loggedIn ? (
-        <div>
-          <p>You are logged in.</p>
-          <button onClick={() => setLoggedIn(false)}>Logout</button>
-        </div>
-      ) : (
-        <Login onLogin={handleLogin} />
-      )}
-    </div>
-  );
-}
-
 function Login({ onLogin }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
@@ -29,18 +8,18 @@ function Login({ onLogin }) {
   const handleSubmit = (event) => {
     event.preventDefault();
 
-    localStorage.setItem('username', username); // save username to local storage
-    localStorage.setItem('password', password); // save password to local storage
+    localStorage.setItem('username', username); 
+    localStorage.setItem('password', password); 
 
-    onLogin(username); // call onLogin with the entered username
+    onLogin(username); 
   };
 
-  // check if there are saved login credentials in the local storage
+  
   const savedUsername = localStorage.getItem('username');
   const savedPassword = localStorage.getItem('password');
 
   if (savedUsername && savedPassword) {
-    // if there are saved credentials, try to log in with them automatically
+   
     if (username === '' && password === '') {
       setUsername(savedUsername);
       setPassword(savedPassword);
