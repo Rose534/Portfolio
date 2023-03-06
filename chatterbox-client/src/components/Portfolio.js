b5828463e0a34407307cef984febead0d59cf6d4
rose534 <wambuirose534@gmail.com>
2023-03-07 01:07:47 +0300

    update portfolio

--- chatterbox-client/src/components/Portfolio.js
+++ chatterbox-client/src/components/Portfolio.js
@@ -1,7 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 function Portfolio() {
   const [projects, setProjects] = useState([]);
+  const [showUpdateForm, setShowUpdateForm] = useState(false);
+  const [currentProject, setCurrentProject] = useState(null);
 
   useEffect(() => {
     fetch('https://rose-portfolio-sinatra.onrender.com/projects')
@@ -19,6 +21,31 @@ function Portfolio() {
     });
   };
 
+  const toggleUpdateForm = (project) => {
+    setCurrentProject(project);
+    setShowUpdateForm(!showUpdateForm);
+  };
+
+  const handleUpdate = (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const id = currentProject.id;
+
+    fetch(`https://rose-portfolio-sinatra.onrender.com/projects/${id}`, {
+      method: 'PUT',
+      body: formData,
+    }).then(() => {
+      
+      fetch('https://rose-portfolio-sinatra.onrender.com/projects')
+        .then(response => response.json())
+        .then(data => {
+          setProjects(data);
+          setShowUpdateForm(false);
+        });
+    });
+  };
+
   return (
     <div className="container">
       <h1 id="illustrations" className="text-center mb-5">Illustrations By Rose</h1>
@@ -28,12 +55,20 @@ function Portfolio() {
             <div className="project-container">
               <img className="project-image img-fluid" src={project.image_url} alt={project.title} />
               <div className="project-overlay">
-                <button id="dltbtn" className="delete-btn" onClick={() => handleDelete(project.id)}>X</button>
+                <button className="update-btn" onClick={() => toggleUpdateForm(project)}>Update</button>
+                <button className="delete-btn" onClick={() => handleDelete(project.id)}>X</button>
               </div>
               <div>
                 <h5>{project.title}</h5>
               </div>
             </div>
+            {showUpdateForm && currentProject && currentProject.id === project.id && (
+              <form className="update-form" onSubmit={handleUpdate}>
+                <input type="text" name="title" defaultValue={currentProject.title} />
+                <input type="text" name="image_url" defaultValue={currentProject.image_url} />
+                <button type="submit">Save</button>
+              </form>
+            )}
           </div>
         ))}
       </div>
