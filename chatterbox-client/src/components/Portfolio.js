2063c500b552a9298484636799c411b2874a04bf
rose534 <wambuirose534@gmail.com>
2023-03-07 00:28:39 +0300

    portfolio page

--- chatterbox-client/src/components/Portfolio.js
+++ chatterbox-client/src/components/Portfolio.js
@@ -1,51 +1,42 @@
 import React, { useState, useEffect } from 'react';
 
 function Portfolio() {
-  const [loading, setLoading] = useState(true);
   const [projects, setProjects] = useState([]);
 
   useEffect(() => {
-    setLoading(true);
     fetch('https://rose-portfolio-sinatra.onrender.com/projects')
       .then(response => response.json())
       .then(data => {
         setProjects(data);
-        setLoading(false);
       });
   }, []);
 
   const handleDelete = (id) => {
-    setLoading(true);
     fetch(`https://rose-portfolio-sinatra.onrender.com/projects/${id}`, {
       method: 'DELETE'
     }).then(() => {
       setProjects(projects.filter(project => project.id !== id));
-      setLoading(false);
     });
   };
 
   return (
     <div className="container">
       <h1 id="illustrations" className="text-center mb-5">Illustrations By Rose</h1>
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <div className="row row-cols-1 row-cols-md-3 g-4">
-          {projects.map(project => (
-            <div className="col-md-4 project" key={project.id}>
-              <div className="project-container">
-                <img className="project-image img-fluid" src={project.image_url} alt={project.title} />
-                <div className="project-overlay">
-                  <button id="dltbtn" className="delete-btn" onClick={() => handleDelete(project.id)}>X</button>
-                </div>
-                <div>
-                  <h5>{project.title}</h5>
-                </div>
+      <div className="row row-cols-1 row-cols-md-3 g-4">
+        {projects.map(project => (
+          <div className="col-md-4 project" key={project.id}>
+            <div className="project-container">
+              <img className="project-image img-fluid" src={project.image_url} alt={project.title} />
+              <div className="project-overlay">
+                <button id="dltbtn" className="delete-btn" onClick={() => handleDelete(project.id)}>X</button>
+              </div>
+              <div>
+                <h5>{project.title}</h5>
               </div>
             </div>
-          ))}
-        </div>
-      )}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
